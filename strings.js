db0ce2c9458798452ff92bae962916b75daf54ac
Glenn Spinner <glenngineer1@gmail.com>
2016-05-31 13:45:12 -0500

    getting started

---
+++ strings.js
@@ -0,0 +1,21 @@
+var inputEl = document.getElementById("inputForLetters");
+
+var inputValue = inputEl.value;
+
+
+function reversal() {
+
+}
+
+function alphabits() {
+
+}
+
+function palindrome() {
+
+}
+
+var testString = "";
+reversal(testString);
+alphabits(testString);
+palindrome(testString);
